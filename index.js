12ff57b6ab942604b38657095ad34cd9ac672c80
yuanqing <hello@yuanqing.sg>
2015-01-01 11:02:22 +0800

    Allow trailing 0 in `path` for the `get` method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var isInt = require('numero').isInt;
+var parseInteger = require('numero').parseInteger;
 var check = require('cheque');
 
 var trimKey = function(key) {
@@ -19,21 +19,20 @@ module.exports.set = function(obj, path, val) {
   // traverse `obj` using `path` (up to the second-last key), updating `o` along the way
   var o = obj;
   var i = -1;
-  var len = path.length-1; // second-last key
+  var len = path.length-1;
   while (++i < len) {
     var key = trimKey(path[i]);
     if (!(key in o)) {
       // current `key` does not exist; set it to either [] or {} by looking at `nextKey`
       var nextKey = trimKey(path[i+1]);
-      o[key] = isInt(nextKey) ? [] : {};
+      o[key] = parseInteger(nextKey) !== null ? [] : {};
     }
     o = o[key];
   }
 
-  // `o` is the
+  // finally set the `val`
   o[trimKey(path[len])] = val;
 
-  // return the original `obj`
   return obj;
 
 };
@@ -54,13 +53,19 @@ module.exports.get = function(obj, path) {
   while (++i < len) {
     var key = trimKey(path[i]);
     // update `obj`
-    if ((check.isObject(obj) || check.isArray(obj)) && key in obj) {
-      obj = obj[key];
-      continue;
+    if ((check.isObject(obj) || check.isArray(obj))) {
+      if (key in obj) {
+        obj = obj[key];
+        continue;
+      }
+    } else if (i === len-1 && (key === 0 || key === '0')) {
+      // here, `obj` is neither object nor array; if we are at the last key in `path`,
+      // allow a trailing '0' eg. get({ foo: 'bar' }, 'foo.0') => 'bar'
+      break;
     }
-    // it must be that `path` is invalid; exit
     return undefined;
   }
+
   return obj;
 
 };
--- test/get.js
+++ test/get.js
@@ -1,50 +1,63 @@
 'use strict';
 
 var test = require('tape');
-var fn = require('..').get;
+var get = require('..').get;
 
-var arr = [ [ 'foo', 'bar' ] ];
+var arr = [
+  ['foo', 'bar'],
+  'baz'
+];
+
 var obj = {
   foo: {
     bar: [
       { baz: 'Hello' },
-      { qux: 'World' }
+      { qux: ['World'] }
     ]
   }
 };
 
 test('get the value for a valid `path` (integer or string)', function(t) {
 
-  t.equal(fn(arr, 0), arr[0]);
-  t.equal(fn(arr, '0'), arr[0]);
-  t.equal(fn(arr, '0.1'), arr[0][1]);
-  t.equal(fn(arr, ' 0 . 1 '), arr[0][1]);
+  t.equal(get(arr, 0), arr[0]);
+  t.equal(get(arr, '0'), arr[0]);
+  t.equal(get(arr, '0.1'), arr[0][1]);
+  t.equal(get(arr, '1.0'), arr[1]);
+  t.equal(get(arr, ' 1 . 0 '), arr[1]);
+  t.equal(get(arr, '0.0.0'), arr[0][0]);
 
-  t.equal(fn(obj, 'foo'), obj.foo);
-  t.equal(fn(obj, 'foo.bar'), obj.foo.bar);
-  t.equal(fn(obj, 'foo.bar.0'), obj.foo.bar[0]);
-  t.equal(fn(obj, ' foo . bar . 0 '), obj.foo.bar[0]);
-  t.equal(fn(obj, 'foo.bar.1.qux'), obj.foo.bar[1].qux);
+  t.equal(get(obj, 'foo'), obj.foo);
+  t.equal(get(obj, 'foo.bar'), obj.foo.bar);
+  t.equal(get(obj, 'foo.bar.0'), obj.foo.bar[0]);
+  t.equal(get(obj, ' foo . bar . 0 '), obj.foo.bar[0]);
+  t.equal(get(obj, 'foo.bar.0.baz'), obj.foo.bar[0].baz);
+  t.equal(get(obj, 'foo.bar.0.baz.0'), obj.foo.bar[0].baz);
+  t.equal(get(obj, 'foo.bar.1.qux.0'), obj.foo.bar[1].qux[0]);
+  t.equal(get(obj, 'foo.bar.1.qux.0.0'), obj.foo.bar[1].qux[0]);
 
   t.end();
 
 });
 
-test('get the value for a valid `path` (integer or string)', function(t) {
+test('get the value for a valid `path` (array)', function(t) {
 
-  t.equal(fn(arr, [ 0 ]), arr[0]);
-  t.equal(fn(arr, [ '0' ]), arr[0]);
-  t.equal(fn(arr, [ 0, 1 ]), arr[0][1]);
-  t.equal(fn(arr, [ '0', '1' ]), arr[0][1]);
-  t.equal(fn(arr, [ ' 0 ', ' 1 ' ]), arr[0][1]);
+  t.equal(get(arr, [ 0 ]), arr[0]);
+  t.equal(get(arr, [ '0' ]), arr[0]);
+  t.equal(get(arr, [ 0, 1 ]), arr[0][1]);
+  t.equal(get(arr, [ 0, '1' ]), arr[0][1]);
+  t.equal(get(arr, [ '1', 0 ]), arr[1]);
+  t.equal(get(arr, [ ' 1 ', ' 0 ' ]), arr[1]);
+  t.equal(get(arr, [ 0, 0, 0 ]), arr[0][0]);
 
-  t.equal(fn(obj, [ 'foo' ]), obj.foo);
-  t.equal(fn(obj, [ 'foo', 'bar' ]), obj.foo.bar);
-  t.equal(fn(obj, [ 'foo', 'bar', '0' ]), obj.foo.bar[0]);
-  t.equal(fn(obj, [ 'foo', 'bar', 0 ]), obj.foo.bar[0]);
-  t.equal(fn(obj, [ ' foo ', ' bar ', ' 0 ' ]), obj.foo.bar[0]);
-  t.equal(fn(obj, [ 'foo', 'bar', 1, 'qux' ]), obj.foo.bar[1].qux);
-  t.equal(fn(obj, [ 'foo', 'bar', '1', 'qux' ]), obj.foo.bar[1].qux);
+  t.equal(get(obj, [ 'foo' ]), obj.foo);
+  t.equal(get(obj, [ 'foo', 'bar' ]), obj.foo.bar);
+  t.equal(get(obj, [ 'foo', 'bar', 0 ]), obj.foo.bar[0]);
+  t.equal(get(obj, [ 'foo', 'bar', '0' ]), obj.foo.bar[0]);
+  t.equal(get(obj, [ ' foo ', ' bar ', ' 0 ' ]), obj.foo.bar[0]);
+  t.equal(get(obj, [ 'foo', 'bar', 0, 'baz' ]), obj.foo.bar[0].baz);
+  t.equal(get(obj, [ 'foo', 'bar', '0', 'baz', 0 ]), obj.foo.bar[0].baz);
+  t.equal(get(obj, [ 'foo', 'bar', '1', 'qux', '0' ]), obj.foo.bar[1].qux[0]);
+  t.equal(get(obj, [ 'foo', 'bar', '1', 'qux', 0, '0' ]), obj.foo.bar[1].qux[0]);
 
   t.end();
 
@@ -52,14 +65,19 @@ test('get the value for a valid `path` (integer or string)', function(t) {
 
 test('returns `undefined` if `path` is invalid', function(t) {
 
-  t.equal(fn(arr, -1), undefined);
-  t.equal(fn(arr, 2), undefined);
+  t.equal(get(arr, -1), undefined);
+  t.equal(get(arr, 2), undefined);
+  t.equal(get(arr, '1.0.0'), undefined);
+  t.equal(get(arr, 'invalid'), undefined);
 
-  t.equal(fn(obj, []), undefined);
-  t.equal(fn(obj, ''), undefined);
-  t.equal(fn(obj, 'invalid'), undefined);
-  t.equal(fn(obj, 'foo.invalid'), undefined);
-  t.equal(fn(obj, 'foo.bar.0.baz.invalid'), undefined);
+  t.equal(get(obj, []), undefined);
+  t.equal(get(obj, ''), undefined);
+  t.equal(get(obj, 0), undefined);
+  t.equal(get(obj, 'invalid'), undefined);
+  t.equal(get(obj, 'foo.0'), undefined);
+  t.equal(get(obj, 'foo.bar.2'), undefined);
+  t.equal(get(obj, 'foo.bar.0.baz.invalid'), undefined);
+  t.equal(get(obj, 'foo.bar.0.baz.0.invalid'), undefined);
 
   t.end();
 
